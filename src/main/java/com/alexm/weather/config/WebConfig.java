ac172232f4f4dd90a14964e49cf4221edf9934ac
Aliaksei Mironchyk <alexm68007@gmail.com>
2023-02-14 18:11:55 +0300

    moved to thymeleaf

---
+++ src/main/java/com/alexm/weather/config/WebConfig.java
@@ -0,0 +1,45 @@
+package com.alexm.weather.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.spring6.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+public class WebConfig implements WebMvcConfigurer {
+    @Bean
+    public ClassLoaderTemplateResolver templateResolver() {
+
+        var templateResolver = new ClassLoaderTemplateResolver();
+
+        templateResolver.setPrefix("templates/");
+        templateResolver.setCacheable(false);
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode("HTML5");
+        templateResolver.setCharacterEncoding("UTF-8");
+
+        return templateResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+
+        var templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+
+        return templateEngine;
+    }
+
+    @Bean
+    public ViewResolver viewResolver() {
+
+        var viewResolver = new ThymeleafViewResolver();
+
+        viewResolver.setTemplateEngine(templateEngine());
+        viewResolver.setCharacterEncoding("UTF-8");
+
+        return viewResolver;
+    }
+}
--- src/main/java/com/alexm/weather/controllers/WeatherReportController.java
+++ src/main/java/com/alexm/weather/controllers/WeatherReportController.java
@@ -2,17 +2,27 @@ package com.alexm.weather.controllers;
 
 import com.alexm.weather.services.WeatherReportService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
-@RestController
+@Controller
 public class WeatherReportController {
     @Autowired
     WeatherReportService weatherReportService;
 
+    @Autowired
+    private TemplateEngine templateEngine;
+
     @GetMapping("/")
-    public ResponseEntity answer() {
-        return ResponseEntity.ok(weatherReportService.returnTemperature());
+    public String answer() {
+        return "index";
+    }
+
+    @ModelAttribute("t")
+    public String getT() {
+        return weatherReportService.returnTemperature();
     }
 }
