c8512899fc9d3da1aed7a969d70a77d7a209f958
Aliaksei Mironchyk <alexm68007@gmail.com>
2023-02-25 21:22:36 +0300

    temperature now

--- src/main/java/com/alexm/weather/services/WeatherReportService.java
+++ src/main/java/com/alexm/weather/services/WeatherReportService.java
@@ -5,12 +5,14 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
 @Service
 public class WeatherReportService {
     public String returnTemperature() {
         JSONObject hourly = (JSONObject) OpenMeteoApi.makeApiCall().get("hourly");
 
         JSONArray temperature = (JSONArray) hourly.get("temperature_2m");
-        return temperature.get(24).toString();
+        return temperature.get(LocalDateTime.now().getHour()).toString();
     }
 }
